5eabfe8f89e3aba7082ec96f4e5c163a5f5e696e
Igor Mendes <gr.mendes1@gmail.com>
2020-04-07 21:34:57 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/example/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/example/cursomc/resource/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.example.cursomc.resource;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 //import java.util.ArrayList;
 //import java.util.List;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.dto.CategoriaDTO;
 import com.example.cursomc.services.CategoriaService;
 
 @RestController
@@ -33,6 +36,7 @@ public class CategoriaResource {
 		
 		@RequestMapping(method=RequestMethod.POST)
 		public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+			
 			obj = service.insert(obj);
 			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 			return ResponseEntity.created(uri).build();
@@ -40,6 +44,7 @@ public class CategoriaResource {
 		
 		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 		public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+			
 			obj.setId(id);
 			obj = service.update(obj);
 			return ResponseEntity.noContent().build();
@@ -47,7 +52,17 @@ public class CategoriaResource {
 		
 		@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
 		public ResponseEntity<Void> delete(@PathVariable Integer id){
+			
 			service.delete(id);
 			return ResponseEntity.noContent().build();
+		}
+		
+		//Caso queira mostrar todas as categorias - diferente do primeiro método que busca somente por id  -----------------
+		@RequestMapping(method=RequestMethod.GET)
+		public ResponseEntity<List<CategoriaDTO>> findAll() {
+			
+			List<Categoria> list = service.findAll();
+			List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
 		}
 }
