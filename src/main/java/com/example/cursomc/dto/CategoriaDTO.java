6c3f519dfab5cbc2bd89cd9e1a7d2c25bf9d6524
Igor Mendes <gr.mendes1@gmail.com>
2020-04-15 21:16:38 -0300

    Personalizando o retorno da validação

--- src/main/java/com/example/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/example/cursomc/dto/CategoriaDTO.java
@@ -2,8 +2,9 @@ package com.example.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import com.example.cursomc.domain.Categoria;
 
