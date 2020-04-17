5f04059b7765c647313746d2a3f83d2d1f3054d4
Igor Mendes <gr.mendes1@gmail.com>
2020-04-16 22:37:43 -0300

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/example/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,34 @@
+package com.example.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.example.cursomc.dto.ClienteNewDTO;
+import com.example.cursomc.resource.exceptions.FieldMessage;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipo() == null) {
+			list.add(new FieldMessage("Tipo", "Tipo não pode ser nulo"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		
+		return list.isEmpty();
+	}
+}
